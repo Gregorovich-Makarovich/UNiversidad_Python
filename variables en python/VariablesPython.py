fcb3868a02f043bbadfa366f803060e09ed367d6
Gregorovich <gregorovich.makarovich@gmail.com>
2022-06-15 02:38:41 -0500

    variables en python

---
+++ variables en python/VariablesPython.py
@@ -0,0 +1,37 @@
+#Variables en python
+
+miVariable = 2
+print( miVariable )
+
+miVariable = 10
+print( miVariable )
+
+
+
+#Variables en Python - parte 2
+
+x = 10
+y = 2
+z = x + y
+print(x)
+print(y)
+print(z)
+
+
+
+#Dirección de Memoria y Variables en Python
+#El id nos devuelve la posicion en memoria donde se almacena las variables X, Y, Z
+print(id(x))
+print(id(y))
+print(id(z))
+
+
+
+#Ejercicio propuesto - Uso de variables
+#Declarar 3 variables cuyos valores sean → nombre, telefono, email
+
+nombre = "carlos"
+telefono = 123456789
+email = "carlos@gmail.com"
+
+print(nombre, telefono, email)
